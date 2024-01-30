11fa0cce68ed557076862b671a7ffaadc146b060
George Brova <gbrova@gmail.com>
2024-01-29 20:44:32 -0500

    Make it mostly correct

--- 1brc.cpp
+++ 1brc.cpp
@@ -35,16 +35,67 @@ public:
     int max;
 };
 
+const int CITY_LENGTH = 32;
+
+// Try to write a custom hash function.
+class City
+{
+
+public:
+    int size;
+    char city[CITY_LENGTH];
+
+    City(char city_c[])
+    {
+        memset(city, 0, CITY_LENGTH);
+        for (int i = 0; i < CITY_LENGTH; i++)
+        {
+            char c = city_c[i];
+            city[i] = c;
+            if (c == '\0')
+            {
+                size = i;
+                break;
+            }
+        }
+        // memcpy(city, city_c, CITY_LENGTH);
+    }
+
+    bool operator==(const City &other) const
+    {
+        // return (city == other.city);
+        int rval = memcmp(city, other.city, CITY_LENGTH);
+        // cout << "rval: " << city << " vs " << other.city << " was " << rval << endl;
+        return rval == 0;
+    }
+};
+namespace std
+{
+    template <>
+    struct hash<City>
+    {
+        size_t operator()(const City &city) const
+        {
+            // This approach turns out to be pretty bad..
+            // return city.city[0] << 24 + city.city[1] << 16 + city.city[2] << 8 + city.city[3];
+
+            // Instead, this seems to work pretty well/fast (inspired by https://stackoverflow.com/a/17017281)
+            int res = 17;
+            res = res * 31 + city.city[0];
+            res = res * 31 + city.city[1];
+            res = res * 31 + city.city[2];
+            res = res * 31 + city.city[8];
+            return res;
+        }
+    };
+}
+
 // kcachegrind callgrind.out.11921 and before that, valgrind --tool=callgrind ./helloworld
 // or try gprof
 
-void parse()
+void aggregate(std::unordered_map<City, Summary> &temps)
 {
-    // TODO: split out the huge function, so I can understand where the time is going.
-}
-
-void aggregate(std::unordered_map<std::string, Summary> &temps)
-{
+    // TODO we probably have to sort first!
     // for (std::unordered_map<std::string, Summary>::iterator iter = temps.begin(); iter != temps.end(); ++iter)
     for (const auto &[city, thisSummary] : temps)
     {
@@ -55,7 +106,7 @@ void aggregate(std::unordered_map<std::string, Summary> &temps)
         float max = (float)thisSummary.max / 10;
         float avg = (float)thisSummary.sum / thisSummary.count / 10;
 
-        fmt::print("{}={:.1f}/{:.1f}/{:.1f}\n", city, min, max, avg);
+        fmt::print("{}={:.1f}/{:.1f}/{:.1f}\n", city.city, min, max, avg);
     }
 }
 
@@ -75,53 +126,9 @@ void update(Summary &thisSummary, float tempf)
     }
 }
 
-int str_to_int(std::string line, int start_pos)
-{
-    int tempf = 0;
-    int sign = 1;
-    for (int i = start_pos; i < line.length(); i++)
-    {
-        char c = line.at(i);
-        if (c == '.')
-        {
-            continue;
-        }
-        if (c == '-')
-        {
-            sign = -1;
-            continue;
-        }
-        int d = c - '0';
-        tempf = tempf * 10 + d;
-    }
-    return tempf * sign;
-}
-
-int str_to_int2(std::vector<char> &buffer, int start_pos, int end_pos)
-{
-    // cout << start_pos << ", " << end_pos << endl;
-    int tempf = 0;
-    int sign = 1;
-    for (int i = start_pos; i < end_pos; i++)
-    {
-        char c = buffer[i];
-        if (c == '.')
-        {
-            continue;
-        }
-        if (c == '-')
-        {
-            sign = -1;
-            continue;
-        }
-        int d = c - '0';
-        tempf = tempf * 10 + d;
-    }
-    return tempf * sign;
-}
-
 int read_city(char city[], char *buffer, int start_pos)
 {
+    memset(city, 0, CITY_LENGTH);
     for (int i = start_pos; i < start_pos + 100; i++)
     {
         char c = buffer[i];
@@ -167,78 +174,15 @@ std::tuple<int, int> read_number(char *buffer, int start_pos)
     return {-1, -1};
 }
 
-const int CITY_LENGTH = 32;
-
-class City
-{
-
-public:
-    char city[CITY_LENGTH];
-
-    City(char city_c[])
-    {
-        memcpy(city, city_c, CITY_LENGTH);
-    }
-
-    bool operator==(const City &other) const
-    {
-        // return (city == other.city);
-        return memcmp(city, other.city, CITY_LENGTH);
-    }
-
-    // std::size_t operator()(const City &s) const noexcept
-    // {
-    //     return 0;
-    //     // std::size_t h1 = std::hash<std::string>{}(s.first_name);
-    //     // std::size_t h2 = std::hash<std::string>{}(s.last_name);
-    //     // return h1 ^ (h2 << 1); // or use boost::hash_combine
-    // }
-};
-namespace std
-{
-    template <>
-    struct hash<City>
-    {
-        size_t operator()(const City &city) const
-        {
-            // Compute individual hash values for two data members and combine them using XOR and bit shifting
-            // return ((hash<float>()(k.getM()) ^ (hash<float>()(k.getC()) << 1)) >> 1);
-            // return 0;
-
-            // hash<string> hasher;
-            // size_t hash = hasher(city.city);
-
-            // size_t result;
-            // memcpy(&result, city.city, 8);
-
-            // return (int)city.city;
-
-            // return *(size_t *)city.city;
-
-            // return result;
-            // int sum = city.city[2] + city.city[2];
-            // int foo = city.city[0] * 977 * 1129 + city.city[1] * 727 + city.city[2];
-            // int foo = 0;
-            return city.city[0] << 24 + city.city[1] << 16 + city.city[2] << 8 + city.city[3];
-            // return foo;
-
-            // return city.city[0] << 16 + city.city[1] << 8 + city.city[2];
-            // return city.city.length();
-            // return hash;
-        }
-    };
-}
-
 void remember(char city[], int tempf, std::unordered_map<City, Summary> &temps)
 {
-    // this lookup makes it take 42ms (i.e. +20ms), which seems like a lot
     City city_class = City(city);
 
     auto tuple = temps.find(city_class);
 
-    // everything together is 56ms (i.e. +14ms), which is maybe reasonable
     if (tuple == temps.end())
     {
+        // cout << "new city: " << city << endl;
         // not found
         Summary thisSummary;
         thisSummary.count = 1;
@@ -250,6 +194,9 @@ void remember(char city[], int tempf, std::unordered_map<City, Summary> &temps)
     }
     else
     {
+        // cout << "old city" << city << endl;
+        // TODO understand why new cities do not hit the other branch. What's going on!!
+
         Summary &thisSummary = tuple->second;
         update(thisSummary, tempf);
     }
@@ -263,8 +210,6 @@ std::unordered_map<City, Summary> read_file(char *buffer, std::streamsize start,
 
     for (int i = start; i < end; i++)
     {
-        // only reading takes 12ms
-        // so reading+copying takes 24ms, which is about as good as we can do (we basically copy everything once).
         i = read_city(city, buffer, i);
 
         auto [ii, tempf] = read_number(buffer, i);
@@ -278,8 +223,8 @@ std::unordered_map<City, Summary> read_file(char *buffer, std::streamsize start,
 
 std::tuple<char *, int> mmap_file()
 {
-
-    char *filepath = "./measurements.txt.1m";
+    // TODO: where I am right now: 10m runs in 300ms on 1 thread.
+    char *filepath = "measurements.txt.1m";
     int fd = open(filepath, O_RDONLY);
     if (fd < 0)
     {
@@ -303,17 +248,10 @@ std::tuple<char *, int> mmap_file()
         exit(2);
     }
 
-    // int sum = 0;
-    // for (int i = 0; i < statbuf.st_size; i++)
-    // {
-    //     sum += buffer[i];
-    // }
-    // cout << "got " << sum << endl;
-
     return {buffer, statbuf.st_size};
 }
 
-void combine_results(std::unordered_map<std::string, Summary> results[], int n_results, std::unordered_map<std::string, Summary> combined_results)
+void combine_results(std::unordered_map<City, Summary> results[], int n_results, std::unordered_map<City, Summary> &combined_results)
 {
     for (int i = 0; i < n_results; i++)
     {
@@ -329,8 +267,6 @@ void combine_results(std::unordered_map<std::string, Summary> results[], int n_r
                 new_summary.max = this_summary.max;
                 new_summary.sum = this_summary.sum;
 
-                // cout << "here" << endl;
-                // cout << city << endl;
                 combined_results.insert(std::pair(city, new_summary));
             }
             else
@@ -374,26 +310,16 @@ int main()
         results[i] = promises[i].get();
     }
 
-    // std::unordered_map<std::string, Summary> combined_results;
-    // combine_results(results, n_threads, combined_results);
+    // std::unordered_map<City, Summary> combined_results = read_file(buffer, 0, size, 100);
+
+    std::unordered_map<City, Summary> combined_results;
+    combine_results(results, n_threads, combined_results);
+    cout << "combined_results has size " << combined_results.size() << endl;
+
+    // This prints ~400 lines and can be noisy; maybe comment it out.
     // aggregate(combined_results);
-
-    // or this
-
-    // int stepsize = size / 1;
-    // cout << "before" << endl;
-    // auto future1 = std::async(read_file, 0, stepsize, 100);
-    // std::cout << "start" << endl;
-    // std::unordered_map<std::string, Summary> temps1 = future1.get();
-
-    // or this.
-
-    // std::unordered_map<std::string, Summary> temps = read_file(0, size / 10, 100);
-
-    // TODO(sort first)
-    // aggregate(temps1);
 
     auto stop = chrono::high_resolution_clock::now();
     auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
-    cout << duration.count() << endl;
+    cout << "time elapsed (us): " << duration.count() << endl;
 }
